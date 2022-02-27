3fbd02a6a6968f3b06bfcfbd7a13e032217dd889
RAKSHA V G <vgraksha26@gmail.com>
2022-02-27 12:51:21 +0530

    Add monitoring layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import About from "./pages/About";
+import Monitoring from "./pages/PlantCare/Monitoring";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/about" element={<About />} />
+        <Route path="/monitor" element={<Monitoring />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/PlantCare/Monitoring.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+export default function Monitoring() {
+  return (
+    <Layout>
+      <div>Monitoring</div>
+    </Layout>
+  );
+}
