a4049ab25ae692a8b76e20346a317080d4367560
RAKSHA V G <vgraksha26@gmail.com>
2022-02-27 18:53:57 +0530

    Add Inspection route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import About from "./pages/About";
 import Monitoring from "./pages/PlantCare/Monitoring";
+import Inspection from "./pages/PlantCare/Inspection";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/monitor" element={<Monitoring />} />
+        <Route path="/inspect" element={<Inspection />} />
       </Routes>
     </BrowserRouter>
   );
