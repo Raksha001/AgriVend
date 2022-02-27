c240dcd2579cb66640b1dd3242dba872d09f265b
Elizabeth Subhiksha <esubivictoria129@gmail.com>
2022-02-28 00:48:37 +0530

    Plant description UI

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import About from "./pages/About";
 import Monitoring from "./pages/PlantCare/Monitoring";
+import Plantcare from "./pages/PlantCare/Plantcare";
+import Plantdescription from "./pages/PlantCare/Plantdescription";
 
 export default function App() {
   return (
@@ -12,6 +14,8 @@ export default function App() {
         <Route path="/" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/monitor" element={<Monitoring />} />
+        <Route path="/plantcare" element={<Plantcare />} />
+        <Route path="/plantdescription" element={<Plantdescription />} />
       </Routes>
     </BrowserRouter>
   );
