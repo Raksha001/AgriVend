38d3bc6ee98d9716569212d92f147058881e8d45
RAKSHA V G <vgraksha26@gmail.com>
2022-02-28 19:48:05 +0530

    complete pg done

--- src/pages/Completed.tsx
+++ src/pages/Completed.tsx
@@ -5,21 +5,12 @@ import Complete from "../assets/images/complete.png";
 export default function Completed() {
   return (
     <Layout>
-      <div className="row">
-        <div className="d-flex col-md-4 mt-4 justify-content-start align-items-start">
-          {/* <div className="ms-4">
-            <p className="mt-2 fs-5">Completed</p>
-          </div> */}
-        </div>
-        <div className="d-flex col-md-8 mt-4 justify-content-between align-items-center">
-          <img
-            src={Complete}
-            className="img-fluid "
-            height="55%"
-            width="100%"
-            alt="farm"
-          />
-          <p className="mt-2 fs-5">Completed</p>
+      <div className="d-flex p-5 align-justify-center">
+        <div className="d-flex row col-md-3 mt-5 text-center">
+          <img src={Complete} className="img-fluid" alt="completed" />
+          <h2 className="text-primary mt-3" style={{ letterSpacing: "0.2rem" }}>
+            Completed
+          </h2>
         </div>
       </div>
     </Layout>
