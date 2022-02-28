2de2836d964af4cb0d96c8308136e38bd55c591b
RAKSHA V G <vgraksha26@gmail.com>
2022-02-28 17:51:13 +0530

    update navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,19 +1,32 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import bg from "../assets/images/bg.png";
 
 export default function NavBar() {
   return (
     <>
-      <Navbar bg="primary" expand="lg" variant="dark">
+      <Navbar
+        style={{
+          background: `url("${bg}")no-repeat center/cover`,
+        }}
+        expand="lg"
+        variant="light"
+      >
         <Container fluid>
-          <Navbar.Brand href="#home">AGRIVEND</Navbar.Brand>
+          <Navbar.Brand
+            className="text-white fs-3 fw-bolder"
+            style={{ letterSpacing: "0.4rem" }}
+            href="#home"
+          >
+            AGRIVEND
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="/about">Home</Nav.Link>
-              <Nav.Link href="/plantcare">Plant care</Nav.Link>
-              <Nav.Link href="#">Sell</Nav.Link>
-              <Nav.Link href="#">Sign out</Nav.Link>
+            <Nav className="ms-auto fw-bold">
+              <Nav.Link href="/about">HOME</Nav.Link>
+              <Nav.Link href="/plantcare">PLANT CARE</Nav.Link>
+              <Nav.Link href="#">SELL</Nav.Link>
+              <Nav.Link href="/">SIGN OUT</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
