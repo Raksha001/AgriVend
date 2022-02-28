a68968b10c7778af7c39c0967c8b615e5a416a27
Elizabeth Subhiksha <esubivictoria129@gmail.com>
2022-02-28 19:38:35 +0530

    Complete Page

--- src/pages/Completed.tsx
+++ src/pages/Completed.tsx
@@ -1,37 +1,25 @@
 import React from "react";
 import Layout from "../components/Layout";
-import Farm from "../assets/images/about-farm.svg";
+import Complete from "../assets/images/complete.png";
 
 export default function Completed() {
   return (
     <Layout>
       <div className="row">
         <div className="d-flex col-md-4 mt-4 justify-content-start align-items-start">
-          <div className="ms-4">
-            <h2
-              className="fst-italic fw-bold"
-              color="dark"
-              style={{ letterSpacing: "0.2rem" }}
-            >
-              ‘NEXT GEN FARMING’
-            </h2>
-            <br />
-            <p className="mt-2 fs-5">
-              This is a farm Management site where as <br /> farmers you can
-              know when to soil and when
-              <br /> to toil by using the best features provided
-              <br /> right from selling to buying as well.
-            </p>
-          </div>
+          {/* <div className="ms-4">
+            <p className="mt-2 fs-5">Completed</p>
+          </div> */}
         </div>
-        <div className="d-flex col-md-8 mt-4 justify-content-end align-items-end">
+        <div className="d-flex col-md-8 mt-4 justify-content-between align-items-center">
           <img
-            src={Farm}
+            src={Complete}
             className="img-fluid "
             height="55%"
             width="100%"
             alt="farm"
           />
+          <p className="mt-2 fs-5">Completed</p>
         </div>
       </div>
     </Layout>
